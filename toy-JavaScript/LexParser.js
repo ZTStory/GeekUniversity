0104419f9f4a4d66ecec7ff780fbe1c391f2e918
ZTStory <m18740395237@163.com>
2021-05-23 22:45:36 +0800

    week05 - 语法树解析

--- toy-JavaScript/LexParser.js
+++ toy-JavaScript/LexParser.js
@@ -2,8 +2,8 @@ class XRegExp {
     constructor(source, flag, root = "root") {
         this.table = new Map();
         this.regexp = new RegExp(this.complieRegExp(source, root, 0).source, flag);
-        console.log(this.regexp);
-        console.log(this.table);
+        // console.log(this.regexp);
+        // console.log(this.table);
     }
 
     complieRegExp(source, name, start) {
@@ -56,52 +56,88 @@ const expressObj = {
     Whitespace: / /,
     LineTerminator: /\n/,
     Comments: /\/\*(?:[^*]|\*[^\/])*\*\/|\/\/[^\n]*/,
-    Token: "<Literal>|<Keywords>|<Identifer>|<Punctuator>",
+    Token: "<Literal>|<Keywords>|<Identifier>|<Punctuator>",
     Literal: "<NumericLiteral>|<BooleanLiteral>|<StringLiteral>|<NullLiteral>",
     NumericLiteral: /(?:[1-9][0-9]*|0)(?:\.[0-9]*)?|\.[0-9]+/,
     BooleanLiteral: /true|false/,
     StringLiteral: /\"(?:[^"\n]|\\[\s\S])*\"|\'(?:[^"\n]|\\[\s\S])*\'/,
     NullLiteral: /null/,
-    Identifer: /[a-zA-Z_$][a-zA-Z0-9_$]*/,
-    Keywords: /if|else|for|function|let/,
+    Identifier: /[a-zA-Z_$][a-zA-Z0-9_$]*/,
+    Keywords: /if|else|for|function|let|var/,
     Punctuator: /\?|\+|\-|\:|\,|\(|\<|\+\+|\=\=|\=\>|\=|\*|\.|\)|\[|\]|\;|\{|\}/,
 };
 
 // let xregexp = complieRegExp(expressObj, "InputElement");
 // console.log(new RegExp(xregexp, "g"));
 
-function scan(str) {
+export function* scan(str) {
     let regexp = new XRegExp(expressObj, "g", "InputElement");
     while (regexp.lastIndex < str.length) {
         let r = regexp.exec(str);
         // document.write(r[0]);
-        console.log(r);
+
+        if (r.Whitespace) {
+        } else if (r.LineTerminator) {
+        } else if (r.Comments) {
+        } else if (r.NumericLiteral) {
+            yield {
+                type: "NumericLiteral",
+                value: r[0],
+            };
+        } else if (r.StringLiteral) {
+            yield {
+                type: "StringLiteral",
+                value: r[0],
+            };
+        } else if (r.BooleanLiteral) {
+            yield {
+                type: "BooleanLiteral",
+                value: r[0],
+            };
+        } else if (r.NullLiteral) {
+            yield {
+                type: "NullLiteral",
+                value: null,
+            };
+        } else if (r.Identifier) {
+            yield {
+                type: "Identifier",
+                value: r[0],
+            };
+        } else if (r.Keywords) {
+            yield {
+                type: r[0],
+            };
+        } else if (r.Punctuator) {
+            yield {
+                type: r[0],
+            };
+        } else {
+            throw new Error("unexpectedToken " + r[0]);
+        }
+
         if (!r[0].length) {
             break;
         }
     }
-
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            let cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.innerText = parttern[i * 3 + j] == 2 ? "❌" : pattern[i * 3 + j] == 1 ? "⭕️" : "";
-            cell.addEventListener("click", () => userMove(j, i));
-            board.appendChild(cell);
-        }
-        board.appendChild(document.createElement("br"));
-    }
+    yield {
+        type: "EOF",
+    };
 }
 
-scan(`
-    for(let i = 0; i < 3; i++) {
-        for(let j = 0; j < 3; j++) {
-            let cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.innerText = parttern[i * 3 + j] == 2 ? "❌" : pattern[i * 3 + j] == 1 ? "⭕️" : "";
-            cell.addEventListener("click", () => userMove(j, i));
-            board.appendChild(cell);
-        }
-        board.appendChild(document.createElement("br"));
+let source = `
+for(let i = 0; i < 3; i++) {
+    for(let j = 0; j < 3; j++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.innerText = parttern[i * 3 + j] == 2 ? "❌" : pattern[i * 3 + j] == 1 ? "⭕️" : "";
+        cell.addEventListener("click", () => userMove(j, i));
+        board.appendChild(cell);
     }
-`);
+    board.appendChild(document.createElement("br"));
+}
+`;
+
+// for (const element of scan(source)) {
+//     // console.log(element);
+// }
---
+++ toy-JavaScript/SyntaxParser.js
@@ -0,0 +1,121 @@
+import { scan } from "./LexParser.js";
+
+let syntax = {
+    Program: [["StatementList", "EOF"]],
+    StatementList: [["Statement"], ["StatementList", "Statement"]],
+    Statement: [["ExpressionStatement"], ["IfStatement"], ["VariableDeclaration"], ["FunctionDeclaration"]],
+    IfStatement: [["if", "(", "Expression", ")", "Statement"]],
+    VariableDeclaration: [["var", "Identifier", ";"]],
+    FunctionDeclaration: [["function", "Identifier", "(", ")", "{", "Statement", "}"]],
+    ExpressionStatement: [["Expression", ";"]],
+    Expression: [["AdditiveExpression"]],
+    AdditiveExpression: [["MultiplicativeExpression"], ["AdditiveExpression", "+", "MultiplicativeExpression"], ["AdditiveExpression", "-", "MultiplicativeExpression"]],
+    MultiplicativeExpression: [["PrimaryExpression"], ["MultiplicativeExpression", "*", "PrimaryExpression"], ["MultiplicativeExpression", "/", "PrimaryExpression"]],
+    PrimaryExpression: [["(", "Expression", ")"], ["Literal"], ["Identifier"]],
+    Literal: [["Number"]],
+};
+
+let hash = {};
+
+function closure(state) {
+    hash[JSON.stringify(state)] = state;
+    let queue = [];
+    for (const symbol in state) {
+        if (symbol.match(/^\$/)) {
+            return;
+        }
+        queue.push(symbol);
+    }
+    // 广度优先搜索
+    while (queue.length) {
+        let symbol = queue.shift();
+        // console.log(symbol);
+        if (syntax[symbol]) {
+            for (const rule of syntax[symbol]) {
+                if (!state[rule[0]]) {
+                    queue.push(rule[0]);
+                }
+                let current = state;
+                for (const part of rule) {
+                    if (!current[part]) {
+                        current[part] = {};
+                    }
+                    current = current[part];
+                }
+                current.$reduceType = symbol;
+                current.$reduceLength = rule.length;
+            }
+        }
+    }
+
+    for (const symbol in state) {
+        if (symbol.match(/^\$/)) {
+            return;
+        }
+        if (hash[JSON.stringify(state[symbol])]) {
+            state[symbol] = hash[JSON.stringify(state[symbol])];
+        } else {
+            closure(state[symbol]);
+        }
+    }
+}
+
+let end = {
+    $isEnd: true,
+};
+
+let start = {
+    Program: end,
+};
+
+closure(start);
+
+let source = `
+    var a;
+    var b;
+`;
+
+function parse(source) {
+    let stack = [start];
+    let symbolStack = [];
+    function reduce() {
+        let state = stack[stack.length - 1];
+
+        if (state.$reduceType) {
+            let children = [];
+            for (let i = 0; i < state.$reduceLength; i++) {
+                stack.pop();
+                children.push(symbolStack.pop());
+            }
+            let newSymbol = {
+                type: state.$reduceType,
+                children: children.reverse(),
+            };
+            // create a non-terminal symbol and shift it
+            return newSymbol;
+        } else {
+            throw new Error("unexpected token");
+        }
+    }
+
+    function shift(symbol) {
+        let state = stack[stack.length - 1];
+        if (symbol.type in state) {
+            stack.push(state[symbol.type]);
+            symbolStack.push(symbol);
+        } else {
+            // reduce to non-terminal symbols
+            shift(reduce());
+            shift(symbol);
+        }
+    }
+
+    for (const symbol of scan(source)) {
+        shift(symbol);
+        console.log(symbol);
+    }
+
+    console.log(reduce());
+}
+
+parse(source);
