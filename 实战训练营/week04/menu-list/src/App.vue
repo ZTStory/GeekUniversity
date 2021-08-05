f9766a51a72c5a8b81ff98d49a975c2c59916a66
ZTStory <m18740395237@163.com>
2021-08-05 11:03:30 +0800

    week04

--- 实战训练营/week04/menu-list/src/App.vue
+++ 实战训练营/week04/menu-list/src/App.vue
@@ -1,58 +1,62 @@
 <template>
-  <div class="menu-list">
-    <div :class="{ active: item.isSelect }" v-for="(item, index) in list" :key="index" @click="clickArticle(item)">
-      {{item.title}}
+    <div class="menu-list">
+        <div :class="{ active: item.isSelect }" v-for="(item, index) in list" :key="index" @click="clickArticle(item)">
+            {{ item.title }}
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
 export default {
-  name: 'App',
-  data() {
-    return {
-      list: []
-    }
-  },
-  mounted() {
-    axios.get("/api/article/list/0/json", {
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      }
-    }).then(res => {
-      if (res.data.data.datas) {
-        this.list = res.data.data.datas;
-      }
-    })
-  },
-  methods: {
-    clickArticle(item) {
-      this.list.forEach(element => {
-        element.isSelect = false;
-      });
-      item.isSelect = true;
-      console.log("文章信息：", item.link);
-
-      this.$customStore.setGlobalState({
-        articleSrc: item.link
-      })
-    }
-  },
-}
+    name: "App",
+    data() {
+        return {
+            list: [],
+        };
+    },
+    mounted() {
+        axios
+            .get("/api/article/list/0/json", {
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                },
+            })
+            .then((res) => {
+                if (res.data.data.datas) {
+                    this.list = res.data.data.datas;
+                }
+            });
+    },
+    methods: {
+        clickArticle(item) {
+            this.list.forEach((element) => {
+                element.isSelect = false;
+            });
+            item.isSelect = true;
+            console.log("文章信息：", item.link);
+            history.pushState(null, "文字详情", "/content");
+            setTimeout(() => {
+                this.$customStore.setGlobalState({
+                    articleSrc: item.link,
+                });
+            }, 300);
+        },
+    },
+};
 </script>
 
 <style>
-.menu-list div{
-  color: #333;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 30px;
-  padding: 5px 0;
-  border-bottom: 1px solid #e3e3e3;
+.menu-list div {
+    color: #333;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 30px;
+    padding: 5px 0;
+    border-bottom: 1px solid #e3e3e3;
 }
 .menu-list .active {
-  color: #00a4ff;
+    color: #00a4ff;
 }
 </style>
